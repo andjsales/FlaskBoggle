9bdde15ff5488254f196645431ef553f4d3bd4ad
andrew.sales <andjsales@gmail.com>
2023-10-11 15:57:36 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, session, jsonify
+from boggle import Boggle
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+
+@app.route('/')
+def home():
+    """Show and generate board."""
+
+    board = Boggle().make_board()
+    session["board"] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template('index.html', board=board, highscore=highscore, nplays=nplays)
+
+
+@app.route("/check-word")
+def check_word():
+    """Check if word is in dictionary."""
+
+    word = request.args["word"]
+    board = session["board"]
+    response = Boggle().check_valid_word(board, word)
+
+    return jsonify({'result': response})
+
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    """Receive score, update nplays, update high score if appropriate."""
+
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
